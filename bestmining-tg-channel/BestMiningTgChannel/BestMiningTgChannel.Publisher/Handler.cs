f2aed9de07a53a63a127a593d2da98be0b47294b
simetrio <simetrio@yandex.com>
2024-05-05 15:58:55 +0500

    Заготовка проекта для бота

---
+++ bestmining-tg-channel/BestMiningTgChannel/BestMiningTgChannel.Publisher/Handler.cs
@@ -0,0 +1,138 @@
+﻿using System.Text.Json;
+
+namespace BestMiningTgChannel.Publisher;
+
+public class Handler : HandlerBase
+{
+    protected override string HandleRequest(string body, RequestData? requestData)
+    {
+        return "Hi!";
+    }
+}
+
+#region Base
+
+public abstract class HandlerBase
+{
+    public Response FunctionHandler(string body)
+    {
+        Logger.Debug(body);
+
+        var request = GetRequest(body);
+        var response = HandleRequest(request);
+
+        return new Response(200, response);
+    }
+
+    private string HandleRequest(Request request)
+    {
+        var requestData = Parse(request.body);
+        return HandleRequest(request.body, requestData);
+    }
+
+    protected abstract string HandleRequest(string request, RequestData? requestData);
+
+    private Request GetRequest(string body)
+    {
+        return body.Contains("httpMethod")
+                ? JsonSerializer.Deserialize<Request>(body)!
+                : new Request { body = body, };
+    }
+
+    private RequestData? Parse(string body)
+    {
+        try
+        {
+            var requestDataDto = JsonSerializer.Deserialize<RequestDataDto>(body)!;
+
+            return new RequestData
+            {
+                Action = Enum.Parse<Action>(requestDataDto.Action),
+                Command = requestDataDto.Command,
+            };
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+
+    protected string SafeExecute(Func<string> func, Func<string>? onError = null)
+    {
+        try
+        {
+            return func();
+        }
+        catch (Exception e)
+        {
+            Logger.Log(e.Message);
+            Logger.Log(e.StackTrace!);
+
+            return onError?.Invoke() ?? "Sorry! I have an error(";
+        }
+    }
+}
+
+public enum Action
+{
+    Send,
+}
+
+public class Request
+{
+    public string httpMethod { get; set; } = null!;
+    public string body { get; set; } = null!;
+}
+
+public class RequestData
+{
+    public Action Action { get; set; }
+
+    public string Command { get; set; } = null!;
+}
+
+public class RequestDataDto
+{
+    public string Action { get; set; } = null!;
+
+    public string Command { get; set; } = null!;
+}
+
+public class Response
+{
+    public Response(int statusCode, string body)
+    {
+        StatusCode = statusCode;
+        Body = body;
+    }
+
+    public int StatusCode { get; set; }
+    public string Body { get; set; }
+}
+
+public static class Logger
+{
+    public static bool IsDebug { get; set; }
+
+    public static void Debug(string message)
+    {
+        if (IsDebug)
+        {
+            Console(message);
+        }
+    }
+
+    public static void Console(string message)
+    {
+        message = $"{DateTime.Now} - {message}";
+
+        System.Console.WriteLine(message);
+    }
+
+    public static void Log(string message)
+    {
+        Console(message);
+    }
+}
+
+#endregion
---
+++ bestmining-tg-channel/BestMiningTgChannel/BestMiningTgChannel.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace BestMiningTgChannel.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
