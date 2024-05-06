129ecc84f8f5d64276d1032432cd9157460deedf
simetrio <simetrio@yandex.com>
2024-05-06 23:28:49 +0500

    После закрыьтя больше не показываю

--- docs/js/site.js
+++ docs/js/site.js
@@ -1,5 +1,16 @@
+const tgModalName = 'tg-banner-closed-5';
+
 function openTgModal() {
     window.setTimeout(() => {
+        var closed = localStorage.getItem(tgModalName);
+        if (!!closed) {
+            return;
+        }
+
         document.getElementById('show-tg-modal').click();
     }, 2000);
+}
+
+function closeTgModal() {
+    localStorage.setItem(tgModalName, 'true');
 }
